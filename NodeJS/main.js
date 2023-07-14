5cc0156879061b31489115eeec05aba64985d17a
locdv <duongvinhloc1@gmail.com>
2023-07-14 15:21:31 +0900

    Delete NodeJS directory

--- NodeJS/main.js
+++
@@ -1,9 +0,0 @@
-const downloadCSV = require('./getCSV');
-const { copyAndchangeFile, checkFileExist, deleteFile } = require('./fileExec');
-async function executeFunctions() {
-    await deleteFile();
-    await downloadCSV();
-    await copyAndchangeFile();
-}
-executeFunctions();
-
